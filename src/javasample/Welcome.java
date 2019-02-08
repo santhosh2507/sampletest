9392cdc2bb9a4402146d6c66f23b846145c53712
santhosh <santhoshkiran25@gmail.com>
2019-02-07 20:05:43 -0500

    First Commit

---
+++ src/javasample/Welcome.java
@@ -0,0 +1,12 @@
+package javasample;
+
+/**
+ * @author SanthoshReddy
+ *
+ */
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/javasample/multiplication.java
@@ -0,0 +1,11 @@
+package javasample;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		int first=10;
+		int second=5;
+		int Mul=first*second;
+		System.out.println("Multiplication is:" +Mul);
+	}
+}
